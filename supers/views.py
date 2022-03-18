84b75a88f2d19b1930e6ce5122999c31888c29a9
Garrett <ga.landin89@gmail.com>
2022-03-18 17:26:58 -0500

    Work in progress - trying to create custom view function responses for Supers views file

--- supers/views.py
+++ supers/views.py
@@ -1,21 +1,40 @@
+from unicodedata import name
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from super_types.models import SuperType
 from .serializers import SuperSerializer
 from .models import Super
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
+        supers_type_param = request.query_params.get('type')
         supers = Super.objects.all()
-        serializer = SuperSerializer(supers, many=True)
-        return Response(serializer.data)
+        custom_response_dictionary = {}
+        if supers_type_param:
+            supers = supers.filter(super_type__type=supers_type_param)
+            serializer = SuperSerializer(supers, many=True)
+            return Response(serializer.data)
+        heroes_list = []
+        villains_list = []
+        custom_response_dictionary = {
+            "Heroes": heroes_list,
+            "Villains": villains_list
+        }
+        for super in supers:             #I need a serializer in here somewhere
+            if SuperType.type == "Hero":
+                heroes_list.append(super)
+            elif SuperType.type == "Villain":
+                villains_list.append(super)
+        return Response(custom_response_dictionary)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def supers_detail(request, pk):
